f1647fbcc18cdee6f695c7abc3a57e87febe20a7
Shamsudiin1 <109215714+Shamsudiin1@users.noreply.github.com>
2022-07-27 00:42:24 +0100

    Delete session 2 directory

--- session 2/Q2.py
+++
@@ -1,4 +0,0 @@
-print(type("1.5"))
-print(type(2.0))
-print(type(528))
-print(type(False))
--- session 2/Q9.py
+++
@@ -1,5 +0,0 @@
-message = 'I am shamsudiin'
-print(message)
-
-message = 'and I am learning to code with Python'
-print(message)
