9de9337c48dd56fb5e6f23f1103fb0fd82edf90d
Shamsudiin1 <109215714+Shamsudiin1@users.noreply.github.com>
2022-07-27 00:50:34 +0100

    Delete session 1 challenges - Copy directory

--- session 1 challenges - Copy/Challenge 2 third output.py
+++
@@ -1,4 +0,0 @@
-name=input("Enter your name:")
-lname=input("Enter your last name:")
-age=input("Enter your age: ")
-print("Your first name is: " + (name.upper()) + " \nYour last name is: " + (lname.upper()) + " \nYour age is: " + age)
--- session 1 challenges - Copy/challenge 1.py
+++
@@ -1,7 +0,0 @@
-orangesPrice = float(input("What is the price of the oranges?"))
-
-percentageDiscount = float(input("What is the percentage discount?"))
-
-discountedPrice = orangesPrice - orangesPrice * percentageDiscount / 100
-
-print("The discounted price of the oranges is "+ str(discountedPrice))
