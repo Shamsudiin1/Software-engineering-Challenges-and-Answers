e1484eadcf5337f46691247aa9fa655b2e2ff211
Shamsudiin1 <109215714+Shamsudiin1@users.noreply.github.com>
2022-08-16 17:09:59 +0100

    Add files via upload

---
+++ Session 4 - Q5.py
@@ -0,0 +1,4 @@
+Car = {"brand": "Ford", "model": "Mustang", "year": 1964}
+Car.clear()
+print(Car)
+
---
+++ Session 4 - Q8.py
@@ -0,0 +1,3 @@
+colours = (("green", "#008000"), ("blue", "#0000FF"))
+dic = dict(colours)
+print(dic)
